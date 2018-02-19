a4c166ceb68b86660392b23c4e10b81ff7f022b5
f.maleki <fatemeh.maleki87@yahoo.com>
2018-02-19 23:59:53 +0330

    learn vue

---
+++ vueapp01/src/main.js
@@ -0,0 +1,22 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import Header1 from './Header1'
+import router from './router'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
+new Vue({
+ el: '#header1',
+ router,
+ components: { Header1 },
+ template: '<Header1/>'
+})
